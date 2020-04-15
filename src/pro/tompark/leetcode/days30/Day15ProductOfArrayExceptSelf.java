11c74d583ecef90f6b7235a0fdb09937e8715094
tomparkpro <tomparkpro@gmail.com>
2020-04-15 23:31:29 +0200

    Day15 Product Of Array Except Self consider space complexity

--- src/pro/tompark/leetcode/days30/Day15ProductOfArrayExceptSelf.java
+++ src/pro/tompark/leetcode/days30/Day15ProductOfArrayExceptSelf.java
@@ -33,6 +33,24 @@ public class Day15ProductOfArrayExceptSelf {
     public static int[] productExceptSelf(int[] nums) {
         int len = nums.length;
         int[] result = new int[len];
+
+        result[0] = 1;
+        for (int i = 1; i < len; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            result[i] = result[i] * right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+
+    public static int[] productExceptSelf1(int[] nums) {
+        int len = nums.length;
+        int[] result = new int[len];
         int[] left = new int[len];
         int[] right = new int[len];
         Arrays.fill(result, 1);
