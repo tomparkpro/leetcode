1804b05eab4f80c3aabd4b14a2012a04e9e78d36
tomparkpro <tomparkpro@gmail.com>
2020-04-28 23:15:58 +0200

    Day27 Maximal Square
    tried different approach

--- src/pro/tompark/leetcode/days30/Day27MaximalSquare.java
+++ src/pro/tompark/leetcode/days30/Day27MaximalSquare.java
@@ -41,6 +41,41 @@ public class Day27MaximalSquare {
     }
 
     public static int maximalSquare(char[][] matrix) {
+        int row = matrix.length;
+        if (row == 0) return 0;
+        int col = matrix[0].length;
+        if (col == 0) return 0;
+
+        int max = 0;
+        int[] sol = new int[col];
+        int diag = matrix[0][0] == '0' ? 0 : 1;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                int temp = sol[j];
+                if (i == 0 || j == 0) {
+                    if (matrix[i][j] == '1') {
+                        sol[j] = 1;
+                        max = Math.max(max, sol[j]);
+                    } else {
+                        sol[j] = 0;
+                    }
+                } else {
+                    if (matrix[i][j] == '1') {
+                        sol[j] = 1 + Math.min(Math.min(sol[j], sol[j-1]), diag);
+                        max = Math.max(max, sol[j]);
+                    } else {
+                        sol[j] = 0;
+                    }
+                }
+                diag = (j == col - 1) ? sol[0] : temp;
+            }
+        }
+
+        return max * max;
+    }
+
+    public static int maximalSquare2(char[][] matrix) {
         if (matrix.length < 1 || matrix[0].length < 1) {
             return 0;
         }
