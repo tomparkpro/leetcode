ebe0a44fe6376bb47974485c1d21751cd154051b
tomparkpro <tomparkpro@gmail.com>
2020-05-10 12:10:22 +0200

    Day10 Find the Town Judge
    Accepted!

--- src/pro/tompark/leetcode/days30/may/Day10FindTheTownJudge.java
+++ src/pro/tompark/leetcode/days30/may/Day10FindTheTownJudge.java
@@ -1,53 +1,113 @@
 package pro.tompark.leetcode.days30.may;
 
 /**
- * Valid Perfect Square
- * Given a positive integer num, write a function which returns True if num is a perfect square else False.
+ * Find the Town Judge
+ * In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
  *
- * Note: Do not use any built-in library function such as sqrt.
+ * If the town judge exists, then:
+ *
+ * The town judge trusts nobody.
+ * Everybody (except for the town judge) trusts the town judge.
+ * There is exactly one person that satisfies properties 1 and 2.
+ * You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+ *
+ * If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
+ *
  *
  * Example 1:
+ * Input: N = 2, trust = [[1,2]]
+ * Output: 2
  *
- * Input: 16
- * Output: true
  * Example 2:
+ * Input: N = 3, trust = [[1,3],[2,3]]
+ * Output: 3
  *
- * Input: 14
- * Output: false
+ * Example 3:
+ * Input: N = 3, trust = [[1,3],[2,3],[3,1]]
+ * Output: -1
+ *
+ * Example 4:
+ * Input: N = 3, trust = [[1,2],[2,3]]
+ * Output: -1
+ *
+ * Example 5:
+ * Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+ * Output: 3
+ *
+ * Note:
+ * 1 <= N <= 1000
+ * trust.length <= 10000
+ * trust[i] are all different
+ * trust[i][0] != trust[i][1]
+ * 1 <= trust[i][0], trust[i][1] <= N
  */
-public class Day9ValidPerfectSquare {
+public class Day10FindTheTownJudge {
 
     public static void main(String[] args) {
-        int num = 16; // true
-//        num = 14; // false
-//        num = 808201; // true
-        num = 2147395600; // true
+        int N = 2;
+        int[][] trust = {{1,2}};
 
-        System.out.println(isPerfectSquare(num));
+        N = 3;
+        trust = new int[][]{{1,3},{2,3}};
+
+        N = 3;
+        trust = new int[][]{{1,3},{2,3},{3,1}};
+
+        N = 3;
+        trust = new int[][]{{1,2},{2,3}};
+
+        N = 4;
+        trust = new int[][]{{1,3},{1,4},{2,3},{2,4},{4,3}};
+
+        N = 1;
+        trust = new int[][]{};
+
+        System.out.println(findJudge(N, trust));
     }
 
-    public static boolean isPerfectSquare(int num) {
-        if (num == 0) return false;
-        if (num == 1) return true;
+    public static int findJudge(int N, int[][] trust) {
+        int trustCount[] = new int[N + 1];
+        int judge = -1;
 
-        boolean isPerfectSquare = false;
-        long lo = 1;
-        long hi = num;
+        for (int i = 0; i < trust.length; i++) {
+            int[] t = trust[i];
+            trustCount[t[0]] -= 1;
+            trustCount[t[1]] += 1;
+        }
 
-        while (lo <= hi) {
-            long mi = lo + (hi - lo) / 2;
-            long result = mi * mi;
-            if (result == num) {
-                isPerfectSquare = true;
+        for (int i = 1; i <= N; i++) {
+            if (trustCount[i] == N - 1) {
+                judge = i;
                 break;
-            } else if (result < num) {
-                lo = (int)mi + 1;
-            } else {
-                hi = (int)mi - 1;
             }
         }
 
-        return isPerfectSquare;
+        return judge;
+    }
+
+    public static int findJudge2(int N, int[][] trust) {
+        if (N == 1 && trust.length == 0) return N;
+
+        int trustTo[] = new int[N + 1];
+        int trustBy[] = new int[N + 1];
+        int judge = -1;
+
+        for (int i = 0; i < trust.length; i++) {
+            int[] t = trust[i];
+            int subject = t[0];
+            int object = t[1];
+            trustTo[subject] += 1;
+            trustBy[object] += 1;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (trustBy[i] == N - 1 && trustTo[i] == 0) {
+                judge = i;
+                break;
+            }
+        }
+
+        return judge;
     }
 
 }
