4a9522b034d10659d6a558db56f66538a417d546
tomparkpro <tomparkpro@gmail.com>
2020-06-03 00:05:04 +0200

    Day2 Delete Node in a Linked List
    Accepted

---
+++ src/pro/tompark/leetcode/days30/june/Day2DeleteNode.java
@@ -0,0 +1,69 @@
+package pro.tompark.leetcode.days30.june;
+
+/**
+ * Delete Node in a Linked List
+ *
+ * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+ *
+ * Given linked list -- head = [4,5,1,9], which looks like following:
+ *
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: head = [4,5,1,9], node = 5
+ * Output: [4,1,9]
+ * Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+ *
+ *
+ * Example 2:
+ *
+ * Input: head = [4,5,1,9], node = 1
+ * Output: [4,5,9]
+ * Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+ *
+ *
+ * Note:
+ *
+ * The linked list will have at least two elements.
+ * All of the nodes' values will be unique.
+ * The given node will not be the tail and it will always be a valid node of the linked list.
+ * Do not return anything from your function.
+ */
+public class Day2DeleteNode {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        ListNode five = new ListNode(5);
+        head.next = five;
+        five.next = new ListNode(1);
+        five.next.next = new ListNode(9);
+
+        deleteNode(five);
+
+        ListNode node = head;
+        while (node != null) {
+            System.out.print(node.val + "-");
+            node = node.next;
+        }
+    }
+
+    public static void deleteNode(ListNode node) {
+        ListNode next = node.next;
+        node.val = next.val;
+        node.next = next.next;
+    }
+
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
